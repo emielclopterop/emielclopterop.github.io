41f3be208867877dd3e805b5b31add46f7abf2c6
emielclopterop <emiel.clopterop@student.arteveldehs.be>
2024-02-14 12:12:43 +0100

    json

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 const projectcontainer = document.getElementById('container');
         const jsonUrl = 'informatie.json';
 
-        // Gebruik de fetch API om de JSON-gegevens op te halen
-        fetch(jsonUrl)
-          .then(response => {
+        async function fetchData() {
+          try {
+            const response = await fetch(jsonUrl);
+
             if (!response.ok) {
               throw new Error(`Netwerkfout: ${response.status}`);
             }
-            return response.json();
-          })
-          .then(data => {
-            // Nu heb je toegang tot de JSON-gegevens
+
+            const data = await response.json();
             console.log(data);
 
-            // Hier kun je de ontvangen gegevens verwerken en weergeven in je HTML
             projectcontainer.textContent = JSON.stringify(data, null, 2);
-          })
-          .catch(error => {
+          } catch (error) {
             console.error('Fout bij het ophalen van JSON:', error);
-          });
+          }
+        }
+
+        fetchData();
